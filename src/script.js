441059f2bb2cdb3aa9887e10aa2596bf9bbdaa43
Nico <juareznico61@gmail.com>
2023-07-29 17:49:05 -0300

    pag proyecto web

--- src/script.js
+++ src/script.js
@@ -1,6 +1,72 @@
 import Swiper from 'swiper';
 
-let swiper = new Swiper(".review-slider", {
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".room-slider", {
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".gallery-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
     spaceBetween: 10,
     grabCursor:true,
     loop:true,
